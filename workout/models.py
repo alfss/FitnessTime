ef9e3cd6969e59e9d93057968eecdebaf6774dea
Sergey Kravchuk <alfss.obsd@gmail.com>
2016-05-30 17:35:17 +0300

    [+] workout

--- workout/models.py
+++ workout/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class SetExercise(models.Model):
+    title = models.CharField( max_length=255 )
+    owner = models.ForeignKey( User )
+
+    unique_together = (("title", "owner"),)
+
+class Exercise(models.Model):
+    priority = models.IntegerField( default=1 ) # for sorting
+    title = models.CharField( max_length=255, unique=True)
+    repeat = models.CharField( max_length=255, default=3)
+    weight = models.FloatField() #in kg
+    rest_time = models.IntegerField() #in seconds
+    set_exercise = models.ForeignKey( 'SetExercise', on_delete = models.CASCADE, )
+
