931218cd9a54479be4c5dc5bbd9fe8be4de282e5
Sergey Kravchuk <alfss.obsd@gmail.com>
2016-05-30 17:48:14 +0300

    [~] fix user ref

--- workout/models.py
+++ workout/models.py
@@ -1,11 +1,11 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 
 class SetExercise(models.Model):
     title = models.CharField( max_length=255 )
-    owner = models.ForeignKey( User )
+    owner = models.ForeignKey( settings.AUTH_USER_MODEL )
 
     unique_together = (("title", "owner"),)
 
