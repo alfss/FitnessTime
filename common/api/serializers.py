9313e2a155954c6c855fcc55ce92e68878d4af84
Sergey Kravchuk <alfss.obsd@gmail.com>
2016-06-14 11:38:03 +0300

    [+] minimal serialize for user

--- common/api/serializers.py
+++ common/api/serializers.py
@@ -2,8 +2,17 @@ from rest_framework import serializers
 from common.models import User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserMiniSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='api-v1:common:user-detail', lookup_field='uuid')
+
     class Meta:
         model = User
-        fields = ('uuid', 'username', 'url', )
+        fields = ('uuid', 'username','url', )
+
+
+class UserSerializer(UserMiniSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api-v1:common:user-detail', lookup_field='uuid')
+
+    class Meta:
+        model = User
+        fields = ('uuid', 'username', 'email', 'first_name', 'last_name' ,'url', )
