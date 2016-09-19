c3855ce2593978cddf7b40e8733cc336ec9dff97
Sergey Kravchuk <alfss.obsd@gmail.com>
2016-09-20 00:43:28 +0300

    [~] add /app* route

--- FitnessTime/urls.py
+++ FitnessTime/urls.py
@@ -27,7 +27,7 @@ from FitnessTime.views import register
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
-    url(r'^app/', views.app, name='main-app'),
+    url(r'^app', views.app, name='main-app'),
     #login
     url(r'^signin/$', login, { 'authentication_form': SiginForm }, name='login'),
     url(r'^register/$', register, name='register'),
